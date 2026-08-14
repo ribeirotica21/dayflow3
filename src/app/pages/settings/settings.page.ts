fd3fda055a5984042e66fc6efbff0cc4086ea3ee
ribeirotica21 <jaydenbrowne414@gmail.com>
2026-08-14 16:59:38 +0000

    Add Settings page with dark mode toggle and clear data option

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,20 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonItem, IonLabel, IonToggle, IonButton,
+  IonIcon, IonList
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trashOutline, moonOutline } from 'ionicons/icons';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    FormsModule,
+    CommonModule,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonItem, IonLabel, IonToggle, IonButton,
+    IonIcon, IonList
+  ]
 })
 export class SettingsPage implements OnInit {
+  darkMode: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private storage: Storage) {
+    addIcons({ trashOutline, moonOutline });
   }
 
+  async ngOnInit() {
+    await this.storage.create();
+    const saved = await this.storage.get('darkMode');
+    this.darkMode = saved || false;
+    this.applyTheme();
+  }
+
+  async toggleDarkMode() {
+    await this.storage.set('darkMode', this.darkMode);
+    this.applyTheme();
+  }
+
+  applyTheme() {
+    document.body.classList.toggle('dark', this.darkMode);
+  }
+
+  async clearAllData() {
+    const confirmed = confirm('This will delete all your tasks and habits. Are you sure?');
+    if (confirmed) {
+      await this.storage.remove('tasks');
+      await this.storage.remove('habits');
+      alert('All data cleared. Restart the app to see the change.');
+    }
+  }
 }
